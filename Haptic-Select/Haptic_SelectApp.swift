7c2ef394cf391cbfac6f9cc5c80e2886e54e9784
Calum Bird <calum@trelent.net>
2023-08-12 16:24:53 -0700

    Initial Commit

---
+++ Haptic-Select/Haptic_SelectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Haptic_SelectApp.swift
+//  Haptic-Select
+//
+//  Created by Calum Bird on 2023-08-12.
+//
+
+import SwiftUI
+
+@main
+struct Haptic_SelectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
